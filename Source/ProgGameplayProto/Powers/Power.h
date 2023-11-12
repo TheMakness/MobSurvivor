4b3dc4ea4f198d111525e0e899f84c3805e7ffdb
TheMakness <dams.6@hotmail.fr>
2023-11-12 11:53:23 +0100

    Add : Base power Class

---
+++ Source/ProgGameplayProto/Powers/Power.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Power.generated.h"
+
+UCLASS()
+class PROGGAMEPLAYPROTO_API APower : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APower();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	virtual void Use();
+
+private:
+	void Cooldown(float DeltaTime);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+
+public:
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	float CountdownStartValue;
+
+protected:
+	float Countdown;
+	bool IsCountdownStart;
+};
