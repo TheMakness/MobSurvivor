aa4f249665fad96b9919bb0875bae08f0c2bec02
TheMakness <dams.6@hotmail.fr>
2023-11-18 09:57:04 +0100

    feat: refactor game instance from BP to C++

---
+++ Source/ProgGameplayProto/MobSurvivorInstance.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "MobSurvivorInstance.generated.h"
+
+class UGameLevelData;
+/**
+ * 
+ */
+UCLASS()
+class PROGGAMEPLAYPROTO_API UMobSurvivorInstance : public 	UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite)
+	UGameLevelData* DA_Level;
+};
