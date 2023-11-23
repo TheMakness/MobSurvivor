85a6a7bc44f4411d535a64949bdf8fb2e6d1c10f
TheMakness <dams.6@hotmail.fr>
2023-11-23 10:53:13 +0100

    wip: add base save class

---
+++ Source/ProgGameplayProto/Save/MobSurvivorSaveGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "MobSurvivorSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROGGAMEPLAYPROTO_API UMobSurvivorSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
