4c0319c11196b9074a9b4cc97be51ff46b24b2c0
Adrien Dudon <dudonadrien@gmail.com>
2023-12-03 11:58:50 +0100

    feat (dasher boss): add boss enemy with a special "dash" attack

---
+++ Source/ProgGameplayProto/Enemies/DasherBossEnemy.cpp
@@ -0,0 +1,141 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DasherBossEnemy.h"
+
+#include "ProgGameplayProto/GameUtils.h"
+#include "ProgGameplayProto/Player/PlayerCharacter.h"
+
+
+// Sets default values
+ADasherBossEnemy::ADasherBossEnemy():
+	DistanceBeforeDash(0),
+	DashExponentialInterpValue(1.2),
+	DashEndLocationOffset(50),
+	DashCooldown(0),
+	DashDuration(0),
+	bDashTimerStarted(false),
+	DashTimer(0),
+	DashCooldownTime(0),
+	bIsCooldownActive(false),
+	AttackRate(5)
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ADasherBossEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ADasherBossEnemy::UpdateAttackCooldown(const float DeltaTime)
+{
+	if (bIsCooldownActive)
+	{
+		DashCooldownTime += DeltaTime;
+		if (DashCooldownTime >= DashCooldown)
+		{
+			bIsCooldownActive = false;
+		}
+	}
+}
+
+void ADasherBossEnemy::Tick(float DeltaTime)
+{
+	UpdateAttackCooldown(DeltaTime);
+	
+	if (bDashTimerStarted)
+	{
+		DashTowardPlayer(DeltaTime);
+	}
+	else
+	{
+		MoveTowardPlayer(DeltaTime);
+	}
+}
+
+void ADasherBossEnemy::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	AttackTimerDelegate.BindUObject(this, &ADasherBossEnemy::TryAttacking, OtherActor);
+	GetWorldTimerManager().SetTimer(AttackTimerHandle, AttackTimerDelegate, AttackRate, true);
+}
+
+void ADasherBossEnemy::NotifyActorEndOverlap(AActor* OtherActor)
+{
+	GetWorldTimerManager().ClearTimer(AttackTimerHandle);
+}
+
+
+void ADasherBossEnemy::ResetAttackCooldown()
+{
+	DashCooldownTime = 0;
+	bIsCooldownActive = true;
+}
+
+void ADasherBossEnemy::ResetDashTimer()
+{
+	DashTimer = 0;
+	bDashTimerStarted = true;
+}
+
+void ADasherBossEnemy::MoveTowardPlayer(float DeltaTime)
+{
+	const APlayerCharacter* Player = UGameUtils::GetMainCharacter();
+
+	if (!IsValid(Player)) return;
+
+	const FVector DeltaLocation = Player->GetActorLocation() - GetActorLocation();
+	const float Distance = DeltaLocation.Length();
+	
+	if (!bIsCooldownActive && Distance < DistanceBeforeDash)
+	{
+		FVector Direction = DeltaLocation;
+		Direction.Z = 0;
+		Direction.Normalize();
+		
+		DashStartLocation = GetActorLocation();
+		DashEndLocation = Player->GetActorLocation() + (Direction * DashEndLocationOffset);
+
+		// TODO: REMOVE IF WON'T BUILD
+		DrawDebugSphere(GetWorld(), Player->GetActorLocation(), 10, 64, FColor::Green, false, DashCooldown - 1);
+		DrawDebugSphere(GetWorld(), DashEndLocation, 10, 64, FColor::Red, false, DashCooldown - 1);
+
+		ResetAttackCooldown();
+
+		ResetDashTimer();
+	}
+	else
+	{
+		FVector Direction = DeltaLocation;
+		Direction.Z = 0;
+		Direction.Normalize();
+
+		const FVector Movement = Direction * MoveSpeed * DeltaTime;
+
+		AddActorWorldOffset(Movement);
+	}
+}
+
+void ADasherBossEnemy::StopDashTimer()
+{
+	DashTimer = 0;
+	bDashTimerStarted = false;
+}
+
+void ADasherBossEnemy::DashTowardPlayer(const float DeltaTime)
+{
+	DashTimer += DeltaTime;
+	
+	const float Alpha = DashTimer / DashDuration;
+
+	const FVector Movement = FMath::InterpEaseInOut(DashStartLocation, DashEndLocation, Alpha, DashExponentialInterpValue);
+	SetActorLocation(Movement);
+
+	// Stop Timer when interpolation is done
+	if (Alpha >= 1.0f)
+	{
+		StopDashTimer();
+	}
+}
---
+++ Source/ProgGameplayProto/Enemies/DasherBossEnemy.h
@@ -0,0 +1,90 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "DasherBossEnemy.generated.h"
+
+/**
+ * Boss enemy that tries to dash into the Player
+ */
+UCLASS(Abstract)
+class PROGGAMEPLAYPROTO_API ADasherBossEnemy : public AEnemy
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditAnywhere, meta = (Units = Centimeters))
+	float DistanceBeforeDash;
+
+	// The exponential value for the EaseInOut interpolation function
+	UPROPERTY(EditAnywhere)
+	float DashExponentialInterpValue;
+	
+	FVector DashStartLocation;
+
+	FVector DashEndLocation;
+
+	UPROPERTY(EditAnywhere)
+	float DashEndLocationOffset;
+	
+	// Time of the dash (the smaller the value, the quicker the dash will be going from Start to End)
+	UPROPERTY(EditAnywhere, meta = (Units = Seconds))
+	float DashDuration;
+
+	// Keep track of running timer value
+	float DashTimer;
+
+	bool bDashTimerStarted;
+
+	// Time before enemy can attack again 
+	UPROPERTY(EditAnywhere, meta = (Units = Seconds))
+	float DashCooldown;
+
+	// Keep track of current cooldown value
+	float DashCooldownTime;
+
+	bool bIsCooldownActive;
+	
+	UPROPERTY(EditAnywhere, meta = (Units = Seconds))
+	float AttackRate;
+
+	FTimerHandle AttackTimerHandle;
+
+	FTimerDelegate AttackTimerDelegate;
+
+public:
+	ADasherBossEnemy();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
+	virtual void NotifyActorEndOverlap(AActor* OtherActor) override;
+
+protected:
+	virtual void MoveTowardPlayer(float DeltaTime) override;
+
+private:
+	void DashTowardPlayer(const float DeltaTime);
+
+	void ResetAttackCooldown();
+
+	/**
+	 * Update cooldown time value and check if cooldown ended
+	 * 
+	 * @param DeltaTime 
+	 */
+	void UpdateAttackCooldown(const float DeltaTime);
+
+	void ResetDashTimer();
+
+	void StopDashTimer();
+
+};
