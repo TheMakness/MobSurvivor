d2c075da6bb0c5502e422c267506a541752f2d94
TheMakness <dams.6@hotmail.fr>
2023-12-04 19:14:41 +0100

    feat: add visual feedback for power cooldown, balance weapon and power

---
+++ Source/ProgGameplayProto/Effects/KnockbackEffect.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectileHitEffect.h"
+#include "KnockbackEffect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROGGAMEPLAYPROTO_API UKnockbackEffect : public UProjectileHitEffect
+{
+	GENERATED_BODY()
+	
+};
