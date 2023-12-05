8f08b28a744958d6094d2dbd64c1d557f89978ac
Adrien Dudon <dudonadrien@gmail.com>
2023-12-05 16:31:52 +0100

    feat: add new specialised character bonus DataAsset

---
+++ Source/ProgGameplayProto/Bonuses/CharacterBonusData.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterBonusData.h"
+
+#include "ProgGameplayProto/Health.h"
+#include "ProgGameplayProto/Player/PlayerCharacter.h"
+
+void UCharacterBonusData::ApplyOnMainCharacter()
+{
+	const APlayerCharacter* MainCharacter = UGameUtils::GetMainCharacter();
+	if (!IsValid(MainCharacter)) return;
+
+	Apply(MainCharacter);
+}
+
+void UCharacterBonusData::Apply(const APlayerCharacter* Character) const
+{
+	UHealth* HealthComponent = Character->GetHealth();
+	if (!IsValid(HealthComponent)) return;
+
+	HealthComponent->AddHealth(HealAmount);
+}
---
+++ Source/ProgGameplayProto/Bonuses/CharacterBonusData.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BonusData.h"
+#include "ProgGameplayProto/GameUtils.h"
+#include "CharacterBonusData.generated.h"
+
+class APlayerCharacter;
+/**
+ * 
+ */
+UCLASS()
+class PROGGAMEPLAYPROTO_API UCharacterBonusData : public UBonusData
+{
+	GENERATED_BODY()
+
+public:
+	// One-shot heal bonus
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Health Bonus")
+	int HealAmount = 0;
+
+	virtual void ApplyOnMainCharacter() override;
+
+	void Apply(const APlayerCharacter* Character) const;
+};
